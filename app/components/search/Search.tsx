01bf486cafa1cf0f918f24c7d3a48547c1bc65d4
Ben Shekhtman <benshekhtman@hotmail.com>
2022-07-11 14:17:01 -0400

    styled search

--- app/components/search/Search.tsx
+++ app/components/search/Search.tsx
@@ -4,16 +4,23 @@ import { SearchProps } from "~/types";
 
 const Search: React.FC<SearchProps> = ({ result }) => {
   return (
-    <ul>
+    <ul className="absolute z-10 bg-white border p-3 mt-2 w-56 text-center left-48 -mt-2">
+      <h2 className="font-prim text-3xl text-dark border-b border-gray-200">
+        Results
+      </h2>
       {result &&
         (result.length > 0 ? (
           result.map(({ name, id }) => (
             <Link to={`/characters/${id}`} key={id}>
-              <li>{name}</li>
+              <li className="hover:bg-gray-200 block my-2 pl-3 py-1 font-prim text-dark w-full text-lg">
+                {name}
+              </li>
             </Link>
           ))
         ) : (
-          <p>No matches found.</p>
+          <p className="block my-2 pl-3 py-1 font-prim text-dark w-full text-lg">
+            No matches found.
+          </p>
         ))}
     </ul>
   );
